c4dc3f7cfb4175115b95b99b0b076f84144b84ef
Leocaliban <leocaliban@gmail.com>
2018-01-01 19:06:16 -0300

    Criação da primeira classe de domínio -Categoria-

--- src/main/java/com/leocaliban/loja/resources/CategoriaResource.java
+++ src/main/java/com/leocaliban/loja/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package com.leocaliban.loja.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.leocaliban.loja.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST Ok!";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1L, "Informática");
+		Categoria cat2 = new Categoria(2L, "Telefonia");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
