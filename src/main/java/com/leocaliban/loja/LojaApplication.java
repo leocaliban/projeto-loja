060d17d43a25a6b793021f632e6f8b2649c42345
Leocaliban <leocaliban@gmail.com>
2018-01-01 03:11:18 -0300

    Projeto Loja Criado.

---
+++ src/main/java/com/leocaliban/loja/LojaApplication.java
@@ -0,0 +1,12 @@
+package com.leocaliban.loja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaApplication.class, args);
+	}
+}
