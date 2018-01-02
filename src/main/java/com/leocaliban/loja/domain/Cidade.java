5ff0e2a1694a8548c4f9b6bc15ceec039f3c7b56
Leocaliban <leocaliban@gmail.com>
2018-01-02 02:57:40 -0300

    Criando entidades Cidade e Estado.

---
+++ src/main/java/com/leocaliban/loja/domain/Cidade.java
@@ -0,0 +1,66 @@
+package com.leocaliban.loja.domain;
+
+import java.io.Serializable;
+
+public class Cidade implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String nome;
+	
+	private Estado estado;
+
+	public Cidade() {
+		
+	}
+	public Cidade(Long id, String nome, Estado estado) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.estado = estado;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Estado getEstado() {
+		return estado;
+	}
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cidade other = (Cidade) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+
+}
