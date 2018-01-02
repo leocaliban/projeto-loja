39a08137613b847113b9e7656ca0ef4d66847b47
Leocaliban <leocaliban@gmail.com>
2018-01-02 03:05:28 -0300

    Mapeando Cidade e Estado.

--- src/main/java/com/leocaliban/loja/domain/Cidade.java
+++ src/main/java/com/leocaliban/loja/domain/Cidade.java
@@ -2,17 +2,29 @@ package com.leocaliban.loja.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Cidade implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
+	
 	private String nome;
 	
+	@ManyToOne
+	@JoinColumn(name="estado_id")
 	private Estado estado;
 
 	public Cidade() {
-		
 	}
 	public Cidade(Long id, String nome, Estado estado) {
 		super();
@@ -62,5 +74,4 @@ public class Cidade implements Serializable {
 		return true;
 	}
 	
-
 }
--- src/main/java/com/leocaliban/loja/domain/Estado.java
+++ src/main/java/com/leocaliban/loja/domain/Estado.java
@@ -4,17 +4,27 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
 public class Estado implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
+	
 	private String nome;
 	
+	@OneToMany(mappedBy="estado")
 	private List<Cidade> cidades = new ArrayList<>();
 	
 	public Estado() {
-		
 	}
 
 	public Estado(Long id, String nome) {
