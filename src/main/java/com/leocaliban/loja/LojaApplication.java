e110920e1f1f401af33e373addd83f9823907b44
Leocaliban <leocaliban@gmail.com>
2018-01-01 23:33:05 -0300

    Criado operação de instanciação com CommandLineRunner

--- src/main/java/com/leocaliban/loja/LojaApplication.java
+++ src/main/java/com/leocaliban/loja/LojaApplication.java
@@ -1,12 +1,30 @@
 package com.leocaliban.loja;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.leocaliban.loja.domain.Categoria;
+import com.leocaliban.loja.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class LojaApplication {
+public class LojaApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LojaApplication.class, args);
+	}
+
+	//Através do CommandLineRunner por padrão será instanciado esses objetos ao iniciar a aplicação
+	@Override
+	public void run(String... arg0) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Telefonia");
+		categoriaRepository.save(Arrays.asList(cat1,cat2));
 	}
 }
