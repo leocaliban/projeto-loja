9347636b0629aec536efae42b873fbd0d36deb7f
Leocaliban <leocaliban@gmail.com>
2018-01-03 03:41:04 -0300

    Criar as classes Cliente e Endereco

---
+++ src/main/java/com/leocaliban/loja/domain/Endereco.java
@@ -0,0 +1,126 @@
+package com.leocaliban.loja.domain;
+
+import java.io.Serializable;
+
+public class Endereco implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String rua;
+	private String numero;
+	private String bairro;
+	private String cep;
+	private String complemento;
+	
+	private Cliente cliente;
+	
+	private Cidade cidade;
+	
+	public Endereco() {
+		
+	}
+
+	public Endereco(Long id, String rua, String numero, String bairro, String cep, String complemento, Cliente cliente,
+			Cidade cidade) {
+		super();
+		this.id = id;
+		this.rua = rua;
+		this.numero = numero;
+		this.bairro = bairro;
+		this.cep = cep;
+		this.complemento = complemento;
+		this.cliente = cliente;
+		this.cidade = cidade;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Cidade getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(Cidade cidade) {
+		this.cidade = cidade;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Endereco other = (Endereco) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+}
